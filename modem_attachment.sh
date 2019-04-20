ca4c785b0d2ff9bddf49a9d62c11ef63ff7da45e
Niven Sookharan <nivensookharan@gmail.com>
2019-04-20 15:28:11 +0200

    wip

--- modem_attachment.sh
+++ modem_attachment.sh
@@ -2,7 +2,7 @@
 
 sleep 5s
 modprobe option
-echo 2001 7e35 > /sys/bus/usb-serial/drivers/option1/new_id
+echo 2001 7e3d > /sys/bus/usb-serial/drivers/option1/new_id
 
 sleep 1s
 logger Launching wvdial script
